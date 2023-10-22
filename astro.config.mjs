ed2e88f0044cfc8f33cb85f17b2e3dedf9936a91
Lucas Garrido <48165310+lucas-garrido@users.noreply.github.com>
2023-10-22 16:44:07 +0000

    add url

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "lasignaturebysabrina.com",
+  site: "https://lasignaturebysabrina.com",
   integrations: [tailwind()],
   vite: {
     ssr: {
